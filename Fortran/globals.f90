ff02d2ad10e00f6a467b9f90184cda529ee46929
Erik Askov Mousing <erikmousing@gmail.com>
2023-09-21 12:26:17 +0200

    Cleaned up globals and input

--- Fortran/globals.f90
+++ Fortran/globals.f90
@@ -1,72 +1,65 @@
 module globals
-  use input
-  implicit none
-  integer, parameter :: dp=kind(0.d0) ! double precision
-  !
-  ! Useful constants:
-  !
 
-  ! Useful mathematical constants:
-  real(dp), parameter :: onethird = 1.d0/3.d0
-  real(dp), parameter :: twothirds = 2.d0/3.d0
-  real(dp), parameter :: threequarters = 3.d0/4.d0
-  real(dp), parameter :: pi = 4*ATAN(1.d0)
+    use input
+    
+    implicit none
+    
+    integer, parameter :: dp = kind(0.d0) ! double precision
 
-  ! Small number to avoid divisions by zero
-  real(dp), parameter :: eps = 1d-200
+    ! Useful constants:
 
-  ! Temperature Q10 corrections (for Q10=2 and Q10=1.5)
-  real(dp) :: fTemp2, fTemp15
-  real(dp), parameter:: Tref = 10. ! Reference temperature
-  
-  !
-  ! Specification of what to do with HTL losses:
-  !
-  real(dp) :: fracHTL_to_N ! Half becomes urine that is routed back to N
-  real(dp) :: fracHTL_to_POM ! Another half is fecal pellets that are routed back to the largest POM size class
-  real(dp) :: rhoCN 
-  !real(dp), parameter :: fracHTL_to_N = 0.5 ! Half becomes urine that is routed back to N
-  !real(dp), parameter :: fracHTL_to_POM = 0.5 ! Another half is fecal pellets that are routed back to the largest POM size class
-  !real(dp), parameter :: rhoCN = 5.68
+    ! Useful mathematical constants:
+    real(dp), parameter :: onethird = 1.d0/3.d0
+    real(dp), parameter :: twothirds = 2.d0/3.d0
+    real(dp), parameter :: threequarters = 3.d0/4.d0
+    real(dp), parameter :: pi = 4*ATAN(1.d0)
 
-  contains
-  
-  ! -----------------------------------------------
-  ! Read in general parameters
-  ! -----------------------------------------------
-  subroutine read_namelist_general()
-    integer :: file_unit,io_err
+    ! Small number to avoid divisions by zero
+    real(dp), parameter :: eps = 1d-200
 
-    namelist /input_general / rhoCN, fracHTL_to_N, fracHTL_to_POM
+    ! Temperature Q10 corrections (for Q10=2 and Q10=1.5)
+    real(dp) :: fTemp2, fTemp15
+    real(dp), parameter:: Tref = 10. ! Reference temperature
 
-    call open_inputfile(file_unit, io_err)
-    read(file_unit, nml=input_general, iostat=io_err)
-    call close_inputfile(file_unit, io_err)
+    !
+    ! Specification of what to do with HTL losses:
+    !
+    real(dp) :: fracHTL_to_N ! Half becomes urine that is routed back to N
+    real(dp) :: fracHTL_to_POM ! Another half is fecal pellets that are routed back to the largest POM size class
+    real(dp) :: rhoCN ! C:N mass ratio of cell [gC/gN]
 
-  end subroutine read_namelist_general
+contains
 
-  ! -----------------------------------------------
-  ! Temperature Q10 function
-  ! -----------------------------------------------
-  function fTemp(Q10, T) result(f)
-    real(dp), intent(in):: Q10, T
-    real(dp):: f
+    subroutine read_namelist_general()
+        !! Read in general parameters
+        integer :: file_unit, io_err
 
-    f = Q10**((T-Tref)/10.)
-  end function fTemp
+        namelist /input_general/ rhoCN, fracHTL_to_N, fracHTL_to_POM
 
-  ! -----------------------------------------------
-  ! Update the temperature corrections only if T has changed
-  ! -----------------------------------------------
-  subroutine updateTemperature(T)
-    real(dp), intent(in) :: T
-    real(dp), save :: Told = -1000.
+        call open_inputfile(file_unit, io_err)
+        read (file_unit, nml=input_general, iostat=io_err)
+        call close_inputfile(file_unit, io_err)
 
-    if (T .ne. Told) then
-      Told = T
-      fTemp2 = fTemp(2.d0, T)
-      fTemp15 = fTemp(1.5d0, T)
-    end if
-  end subroutine updateTemperature
+    end subroutine read_namelist_general
+
+    function fTemp(Q10, T) result(f)
+        !! Temperature Q10 function
+        real(dp), intent(in):: Q10, T
+        real(dp):: f
+
+        f = Q10**((T - Tref)/10.)
+    end function fTemp
+
+    subroutine updateTemperature(T)
+        !! Update the temperature corrections only if T has changed
+        real(dp), intent(in) :: T
+        real(dp), save :: Told = -1000.
+
+        if (T .ne. Told) then
+            Told = T
+            fTemp2 = fTemp(2.d0, T)
+            fTemp15 = fTemp(1.5d0, T)
+        end if
+    end subroutine updateTemperature
 
 end module globals
--- Fortran/input.f90
+++ Fortran/input.f90
@@ -1,40 +1,37 @@
-!
-! Module to handle the reading of the input file
-!
 module input
-  implicit none
-  
-  contains
-  
-  subroutine open_inputfile(file_unit, io_err)
-    integer,  intent(out) :: file_unit, io_err
-  
-    open(newunit=file_unit,action='read', file="../input/input.nlm",iostat=io_err)
-    call check_iostat(io_err, &
-        "Could not open file 'input.nml', perhaps it does not exist?")
+    !! Module to handle the reading of the input file
+    
+    implicit none
 
-  end subroutine open_inputfile
-  
-  subroutine close_inputfile(file_unit, io_err)
-    integer,  intent(in) :: file_unit, io_err
-    call check_iostat(io_err, &
-        "Error reading name-list, please correct file content")
-    close (file_unit)
-  end subroutine close_inputfile
+contains
 
-  ! -----------------------------------------------
-  ! Find problems with input
-  ! -----------------------------------------------
- 
-  subroutine check_iostat(iostat, msg)
-    integer, intent(in) :: iostat
-    character(len=*), intent(in) :: msg
+    subroutine open_inputfile(file_unit, io_err)
+        integer, intent(out) :: file_unit, io_err
 
-    if ( iostat == 0 ) return
+        open (newunit=file_unit, action='read', file="../input/input.nlm", iostat=io_err)
+        call check_iostat(io_err, &
+                          "Could not open file 'input.nml', perhaps it does not exist?")
 
-    write(*,'(a,i0,/,tr3,a)') 'ERROR = ', iostat, msg
-    stop
+    end subroutine open_inputfile
 
-  end subroutine check_iostat
+    subroutine close_inputfile(file_unit, io_err)
+        integer, intent(in) :: file_unit, io_err
+
+        call check_iostat(io_err, &
+                          "Error reading name-list, please correct file content")
+        close (file_unit)
+    end subroutine close_inputfile
+
+    subroutine check_iostat(iostat, msg)
+        ! Find problems with input
+        integer, intent(in) :: iostat
+        character(len=*), intent(in) :: msg
+
+        if (iostat == 0) return
+
+        write (*, '(a,i0,/,tr3,a)') 'ERROR = ', iostat, msg
+        stop
+
+    end subroutine check_iostat
 
 end module input
